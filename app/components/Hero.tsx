f70ea9a141233dbb87b7d49ac0bd07f754030507
FlorianChazizi <chazizi.florian@gmail.com>
2024-12-08 00:37:31 +0200

    hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -14,8 +14,8 @@ const Hero = () => {
 
   return (
     <div>
-      <section className="relative block w-full h-[100vh] text-white items-center justify-center hero">
-        <div className="flex items-center justify-center h-full">
+      <section className="hero">
+        <div className="">
           <div className="text-center hero-content ">
             <h2 className="text-3xl md:text-6xl font-bold mb-4 hero-content shine-text" data-aos="fade-down">
               Building the Future, Restoring the Past
