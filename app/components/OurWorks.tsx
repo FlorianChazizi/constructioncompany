6a4a02ca23edbd6ab2f19e16d9ea12d7bbb53279
FlorianChazizi <chazizi.florian@gmail.com>
2024-12-16 20:12:37 +0200

    our works

--- app/components/OurWorks.tsx
+++ app/components/OurWorks.tsx
@@ -5,7 +5,7 @@ const OurWorks = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100" id="our-work">
       <div className="text-center">
-        <h3 className="text-3xl font-mono font-bold mb-12 text-gray-800">Portfolio</h3>
+        <h3 className="text-3xl font-mono font-bold mb-12 text-gray-800">Οι Δουλιές μας</h3>
         <Carousel />
       </div>
     </div>
