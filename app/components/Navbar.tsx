43571891b0a353bfae8e82e96de5b51820133a3b
FlorianChazizi <chazizi.florian@gmail.com>
2025-01-13 18:52:15 +0200

    Navbar update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,28 @@
 "use client"
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/navbar.css';
 import Image from "next/image";
 import logo from "../public/assets/images/logo.jpg";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
-  const handleLinkClick = () => {
-    setIsOpen(false); // Close the menu
-  };
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const handleScroll = () => setIsScrolled(window.scrollY > 50);
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
 
   return (
-    <nav className="navigation-bar shadow-lg">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <nav
+      className={`navigation-bar ${isScrolled ? "scrolled" : ""} ${
+        isOpen ? "is-open" : ""
+      }`}
+    >      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
             <h1>
@@ -53,10 +61,10 @@ export default function Navbar() {
       {isOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <a href="#our-work" className="block navlinks px-3 py-2 rounded-md text-base font-medium text-center" onClick={handleLinkClick}>Οι Δουλιές μας</a>
-            <a href="#services" className="block navlinks px-3 py-2 rounded-md text-base font-medium text-center" onClick={handleLinkClick}>Υπηρεσίες</a>
-            <a href="#about" className="block navlinks px-3 py-2 rounded-md text-base font-medium text-center" onClick={handleLinkClick}>Ποιοί Ήμαστε</a>
-            <a href="#contact" className="block navlinks px-3 py-2 rounded-md text-base font-medium text-center" onClick={handleLinkClick}>Επικοινωνία</a>
+            <a href="#our-work" className="block navlinks px-3 py-2 rounded-md text-base font-medium text-center"  onClick={() => setIsOpen(false)}>Οι Δουλιές μας</a>
+            <a href="#services" className="block navlinks px-3 py-2 rounded-md text-base font-medium text-center"  onClick={() => setIsOpen(false)}>Υπηρεσίες</a>
+            <a href="#about" className="block navlinks px-3 py-2 rounded-md text-base font-medium text-center"  onClick={() => setIsOpen(false)}>Ποιοί Ήμαστε</a>
+            <a href="#contact" className="block navlinks px-3 py-2 rounded-md text-base font-medium text-center"  onClick={() => setIsOpen(false)}>Επικοινωνία</a>
           </div>
         </div>
       )}
