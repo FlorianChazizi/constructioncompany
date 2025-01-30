d4f35b8656b3c00c8d70761db8ea3810fd050704
FlorianChazizi <chazizi.florian@gmail.com>
2025-01-30 19:07:25 +0200

    link changed

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -21,12 +21,12 @@ const Hero = () => {
         {/* Hero content */}
         <div className="hero-content">
           <h2 className="text-3xl md:text-6xl font-bold mb-4 shine-text" data-aos="fade-down">
-            Κάνουμε τα όνειρα σας πραγματικότητα.
+            Κάνουμε τα όνειρα σας πραγματικότητα
           </h2>
           <h3 className="text-lg md:text-xl text-white" data-aos="fade-up">
             Φτιάχουμε το σπίτι των ονείρων σας.
           </h3>
-          <a href="#services" className="hero-button" data-aos="fade-up" data-aos-delay="200">
+          <a href="#contact" className="hero-button" data-aos="fade-up" data-aos-delay="200">
             Επικοινωνια
           </a>        
         </div>
